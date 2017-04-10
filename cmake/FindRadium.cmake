8b24bdd996283d7c5b9facf5a31ba959a4eed68b
Nicolas Mellado <nmellado0@gmail.com>
2017-04-10 13:00:39 +0200

    Update FindRadium with Radium's latest version Update cmakemodule path wrt to changes in Radium directories

--- cmake/FindRadium.cmake
+++ cmake/FindRadium.cmake
@@ -2,7 +2,6 @@
 # Will define
 # RADIUM_ROOT_DIR : the root of the radium SDK
 # RADIUM_INCLUDE_DIR : the include directory of radium
-# EIGEN_INCLUDE_DIR : the eigen directory
 # RADIUM_PLUGIN_OUTPUT_PATH : output path for radiums plugin
 
 
@@ -27,10 +26,17 @@ IF(NOT RADIUM_ROOT_DIR)
     DOC "The radium engine source folder")
 ENDIF(NOT RADIUM_ROOT_DIR)
 
+
 IF ( RADIUM_ROOT_DIR )
-    SET ( RADIUM_INCLUDE_DIR "${RADIUM_ROOT_DIR}/src")
-    SET ( EIGEN_INCLUDE_DIR "${RADIUM_ROOT_DIR}/3rdPartyLibraries/Eigen")
-    SET ( ASSIMP_INCLUDE_DIR "${RADIUM_ROOT_DIR}/3rdPartyLibraries/Assimp/include")
+    SET ( RADIUM_INCLUDES "${RADIUM_ROOT_DIR}/src")
+
+#    IF(NOT EIGEN3_INCLUDE_DIR)
+#        set(EIGEN3_DIR ${RADIUM_ROOT_DIR}/${CMAKE_BUILD_TYPE}/3rdPartyLibraries/include)
+#    find_package(Eigen3 3.1.2 REQUIRED)
+#    ENDIF(NOT EIGEN3_INCLUDE_DIR)
+    #find_package(Assimp REQUIRED)
+    #find_package(GLBinding REQUIRED)
+
     SET ( RADIUM_PLUGIN_OUTPUT_PATH "${RADIUM_ROOT_DIR}/${CMAKE_BUILD_TYPE}/Plugins")
 
     IF (TARGET radiumCore)
@@ -60,25 +66,27 @@ IF ( RADIUM_ROOT_DIR )
             )
     ENDIF()
 
-    FIND_LIBRARY ( ASSIMP_LIBRARIES
-        NAMES assimp assimp32.lib assimp.lib libassimp.a
-        PATHS ${RADIUM_ROOT_DIR}/${CMAKE_BUILD_TYPE}/lib
-        ${RADIUM_ROOT_DIR}/lib
-        )
-
     SET ( Radium_FOUND TRUE )
+
+    # TODO (Mathias) : verify if cmake recommand this (append the include dir of module dependencis in the include dir of the module header)
+    SET( RADIUM_INCLUDE_DIR)
+    LIST(APPEND RADIUM_INCLUDE_DIR "${RADIUM_INCLUDES}" "${EIGEN3_INCLUDE_DIR}" "${ASSIMP_INCLUDE_DIR}" "${GLBINDING_INCLUDE_DIR}")
+
+    # TODO (Mathias) Like above : is it recommended to add all dependencies here ?
     SET( RADIUM_LIBRARIES )
-    IF ( RA_CORE_LIB AND RA_ENGINE_LIB AND RA_GUIBASE_LIB AND ASSIMP_LIBRARIES)
+    IF ( RA_CORE_LIB AND RA_ENGINE_LIB AND RA_GUIBASE_LIB AND ASSIMP_LIBRARIES AND GLBINDING_LIBRARIES)
        LIST(APPEND RADIUM_LIBRARIES "${RA_CORE_LIB}" "${RA_ENGINE_LIB}" "${RA_GUIBASE_LIB}")
        SET ( Radium_Libs_FOUND TRUE)
-    ENDIF ( RA_CORE_LIB AND RA_ENGINE_LIB AND RA_GUIBASE_LIB AND ASSIMP_LIBRARIES)
+    ENDIF ( RA_CORE_LIB AND RA_ENGINE_LIB AND RA_GUIBASE_LIB AND ASSIMP_LIBRARIES AND GLBINDING_LIBRARIES)
 ENDIF( RADIUM_ROOT_DIR)
 
 IF ( Radium_FOUND )
     IF(NOT Radium_FIND_QUIETLY)
       MESSAGE ( STATUS "Found Radium Engine: ${RADIUM_ROOT_DIR}")
+      MESSAGE ( STATUS "      Eigen3 includes: ${EIGEN3_INCLUDE_DIR}")
       MESSAGE ( STATUS "      Radium libs: ${RADIUM_LIBRARIES}")
       MESSAGE ( STATUS "      Assimp libs: ${ASSIMP_LIBRARIES}")
+      MESSAGE ( STATUS "      GlBinding libs: ${GLBINDING_LIBRARIES}")
     ENDIF(NOT Radium_FIND_QUIETLY)
     IF (NOT Radium_Libs_FOUND)
         MESSAGE( WARNING "Could not find Radium libraries. You must compile them first")
